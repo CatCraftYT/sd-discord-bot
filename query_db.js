16cb479bd8d12fee90b8e40b555b253fcba355da
CatCraftYT <37033401+CatCraftYT@users.noreply.github.com>
2023-01-19 20:49:58 +1030

    added export option to query script

--- query_db.js
+++ query_db.js
@@ -1,5 +1,18 @@
 import * as csv from "./csv.js"
 import * as readline from 'node:readline';
+import { open } from 'node:fs/promises';
+import { appendFile } from 'node:fs';
+
+const FILENAME = "generations.csv";
+
+export async function ConvertFile() {
+    const stream = (await open(FILENAME)).createReadStream();
+    const rl = readline.createInterface(stream);
+    for await (const line of rl) {
+        appendFile("export.json.log", JSON.stringify(JSON.parse(Buffer.from(line.split(",")[1], "base64").toString()), undefined, 4) + "\n", (err) => {if (err !== null) { throw err }});
+    }
+    stream.close();
+}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,6 +22,10 @@ const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 
 while (true) {
     const id = await prompt("> ");
+    if (id === "export") {
+        ConvertFile();
+        continue;
+    }
     const json = await csv.ReadEntry(id);
     console.log(JSON.parse(json));
 }
